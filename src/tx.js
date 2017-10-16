077f98c0e1118392ef6315e46aef6dd8d842569c
holgerd77 <Holger.Drewes@gmail.com>
2017-10-16 13:37:32 +0200

    Removed outdated src files

--- src/tx.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  Buffer: require('buffer'),
-  BN: require('ethereumjs-util').BN,
-  RLP: require('ethereumjs-util').rlp,
-  Tx: require('ethereumjs-tx'),
-  Util: require('ethereumjs-util')
-}
